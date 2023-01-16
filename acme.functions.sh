1243953d6fb81b2a90d316aef81d46e1548d8973
nimpo <dr.mike.jones@gmail.com>
2023-01-16 12:13:13 +0000

    Add functions for monitorCert.sh

--- acme.functions.sh
+++ acme.functions.sh
@@ -196,6 +196,10 @@ function getHTTPHeader () { # Gets a header $1 from file $2 or stdin if $2 not s
 }
 
 #------------Cert Functions
+# getCertHash <file> ; returns openssl hash
+# getIssuerHash
+# isCA
+#
 function getCertHash () {
   openssl x509 -in "$1" -noout -hash 2>/dev/null
 }
@@ -220,7 +224,7 @@ function getCertPrimaryName () {
   [ "$CertName" ] && echo "$CertName" || return 1
 }
 
-stashHashNoClash () {
+function stashHashNoClash () {
   dir="${2:-.}" ; mkdir -p $dir
   hash=`getCertHash "$1"`
   i=0 ; while [ -e "$dir/$hash.$i" ] ; do let $i++ ; done
@@ -236,7 +240,7 @@ stashHashNoClash () {
 #
 # splitCerts [first] [last] ### starting to read at verse 0
 
-splitCerts () { # Take a pem chain on stdin and split it into its components
+function splitCerts () { # Take a pem chain on stdin and split it into its components
 #  [ -r "$1" ] && [ -s "$1" ] || return 1
   FirstCertName="UnNamedCert"
   Split=`mktemp -d -p.`
@@ -310,3 +314,202 @@ function constructChainWithRoots () { # $1 is cert $2... are paths to CA certs o
   done
   for cert in ${localchain[@]} ; do openssl x509 -in $cert ; done
 }
+
+#######
+
+function systemDNSs () { # Return nameservers from system locations starting with upstream ones for systemd resolved
+  local -A USED=()
+  for NS in `cat /run/systemd/resolve/resolv.conf /etc/resolv.conf 2>/dev/null | awk '/^[[:space:]]*nameserver[[:space:]]{1,}[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}[[:space:]]*$/ {print $2}'`
+  do
+    [ ${USED[$NS]} ] && continue
+    USED[$NS]="y" && echo $NS
+  done
+}
+
+
+function DNScheck () { # Attempt to resolve FQDN(s). For Acme HTTP-01 this needs to be resolved on the WAN so callback checks from external services can reach each FQDN via IP(v4 in this case).
+                       # Usage DNScheck [-g] add 8.8.8.8 and 8.8.4.4 to the list of resolvers
+                       #                [-r] add nameservers from /etc/resolv/conf
+                       #                [-s] add nameservers from /run/systemd/resolve/resolv.conf
+                       #                [-R <path>] add nameservers from resolv.conf at <path>
+                       # We could use e.g. 8.8.8.8 as this is what LetsEncrypt uses, so why so complicated? 
+                       # Because 8.8.8.8 isn't always available. Particularly on the author's home setup.
+                       # This is for reasons to do with forcing local resolution of FQDN to reserved 192 addresses on that network and public IP addresses in the wild,
+                       # and so having to sidestep Android's massively inconvenient default to always use 8.8.8.8 as secondary DNS server where home routers only provides one DNS server via DHCP 
+                       # (thanks Netgear for squashing identical secondary DNS!) via DHCP. #Grrr
+  local -a NameServers=()
+  local -a FQDN=()
+  while [ "$1" ]
+  do
+    case "$1" in
+      -g) NameServers+=(8.8.8.8 8.8.4.4) ; shift ;;
+     -ns) shift ; NameServers+=($1) ; shift ;;
+      -r) for ns in `awk '/^[[:space:]]*nameserver[[:space:]]{1,}[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}[[:space:]]*$/ {print $2}' /etc/resolv.conf`
+          do NameServers+=($ns) ; done ; shift ;;
+      -s) for ns in `awk '/^[[:space:]]*nameserver[[:space:]]{1,}[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}[[:space:]]*$/ {print $2}' /run/systemd/resolve/resolv.conf`
+          do NameServers+=($ns) ; done ; shift ;;
+      -R) shift
+          [ ! -e "$1" ] && debug "DNScheck option -R <path>: path must exist" && return 1
+          for ns in `awk '/^[[:space:]]*nameserver[[:space:]]{1,}[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}[[:space:]]*$/ {print $2}' "$1"`
+          do NameServers+=($ns) ; done ; shift ;;
+   [^-]*) if echo $1 | checkFQDN 
+          then FQDN+=($1)
+          else debug "dodgy FQDN "'"'"$1"'"'" in DNScheck" ; return 1
+          fi
+          shift ;;
+       *) debug "Unknown option "'"'"$1"'"'"to DNScheck"; return 1 ;;
+    esac
+  done
+
+  debug "DNScheck for `echo "${FQDN[*]}"|sed -e 's/ /, /g'`. Trying nameservers in this order `echo "${NameServers[*]}"|sed -e 's/ /, /g'`"
+  local fqdn
+  local ns
+  local -A USED # Only check FQDN against each server once
+  local OK=0
+  for fqdn in ${FQDN[@]}
+  do
+    if [ ${#NameServers[@]} -eq 0 ]
+    then
+      [ "`dig "$fqdn" +short`" ] && verbose "Found $fqdn in local resolver" && OK=0 && continue
+    else
+      USED=()
+      for ns in ${NameServers[@]}
+      do
+        [ ${USED[$ns]} ] && continue
+        [ "`dig "@$ns" "$fqdn" +short`" ] && verbose "Found $fqdn in $ns" && continue 2
+        USED[$ns]=y
+      done
+    fi
+    verbose "$fqdn Not found in DNS lookups"
+    OK=256
+  done
+  return $OK
+}
+
+# Check cert name of pem cert in $1 against dns names in $2 -- $N
+#   checkNames <pathToCert> <dnsName> [<dnsname>]...
+#
+checkNames () {
+  MyCERTPATH="$1"
+  local OK=0
+  shift
+  for a in "$@"
+  do
+    echo "$a" | checkFQDN || return 127
+    openssl x509 -in "$MyCERTPATH" -noout || return 127
+    openssl x509 -in "$MyCERTPATH" -noout -ext subjectAltName |grep '^[[:space:]]*DNS:[a-zA-Z0-9_-]*\.[a-zA-Z0-9_.-]*$' |sed -e 's/^[[:space:]]*DNS://' |grep -q "^$a$" && continue
+    openssl x509 -in "$MyCERTPATH" -noout -nameopt multiline -subject |grep '^[[:space:]]*commonName[[:space:]]*=[[:space:]]*[a-zA-Z0-9_-]*\.[a-zA-Z0-9_.-]*$' |sed -e 's/^[[:space:]]*commonName[[:space:]]*=[[:space:]]*//' |grep -q "^$a$" && continue
+    debug "checkNames: Certificate not valid for $a"
+    OK=1
+  done
+  return $OK
+}
+
+# Check Date of $1 certificate
+#   checkDates <pathToCert> [<DaysHeadsUp>]
+#
+checkDates () {
+  debug "checkDates $@"
+  local NOTBEFOREDATE=`openssl x509 -in "$1" -noout -startdate` || return 127
+  local NOTAFTERDATE=`openssl x509 -in "$1" -noout -enddate` || return 127
+  local START=`echo $NOTBEFOREDATE | sed -e 's/.*=//' | xargs -i date -d '{}' +%s`
+  local END=`echo $NOTAFTERDATE | sed -e 's/.*=//' | xargs -i date -d '{}' +%s`
+  local NOW=`date +%s`
+  local HEADSUP=`dc -e "$END ${2:-0} 86400 * - p"` || return 127
+  debug "Not before: $START; Not after: $END; Heads Up: $HEADSUP; Now: $NOW"
+  [ $NOW -ge $END ]     && echo "Expired" && return 1
+  [ $NOW -lt $START ]   && echo "Not Yet Valid" && return 2
+  [ $NOW -gt $HEADSUP ] && echo "Renew" && return
+  echo "OK"
+}
+
+# Function to check Certs and Keys match
+#   checkKeyCertMatch <pathToKey> <pathToCert>
+#   checkCertModsMatch <pathToCert1> <pathToCert2>
+#   checkCertsMatch <pathToCert1> <pathToCert2>
+#
+checkKeyCertMatch () {
+  debug "checkKeyCertMatch $@"
+  ModulusKey=`openssl rsa -in "$1" -noout -modulus` || return 127
+  ModulusCert=`openssl x509 -in "$2" -noout -modulus` || return 127
+  [ "$ModulusCert" ] || return 1
+  [ "$ModulusCert" = "$ModulusKey" ] && return 0
+}
+
+checkCertModsMatch () { # Not necessarily identical certs just identical key material
+  debug "checkKeyCertMatch $@"
+  ModulusCert1=`openssl x509 -in "$1" -noout -modulus` || return 127
+  ModulusCert2=`openssl x509 -in "$2" -noout -modulus` || return 127
+  [ "$ModulusCert1" ] || return 1
+  [ "$ModulusCert1" = "$ModulusCert2" ] && return 0
+}
+
+checkCertsMatch () {
+  debug "checkKeyCertMatch $@"
+  FingerprintCert1=`openssl x509 -in "$1" -noout -SHA256 -fingerprint` || return 127
+  FingerprintCert2=`openssl x509 -in "$2" -noout -SHA256 -fingerprint` || return 127
+  [ "$FingerprintCert1" ] || return 1
+  [ "$FingerprintCert1" = "$FingerprintCert2" ] && return 0
+}
+
+# Checks first cert (first PEM in first cert with system trust roots and optional N untrusted Intermediate CAfiles
+#   checkCertChain <pathToCertUnderTest> <PathToTrustedCAs> [ <pathToUntrustedCAChain> ] ...
+#
+checkCertChain () {
+  [ -f "$2" ] && CAs=("-CAfile" "$2")
+  [ -d "$2" ] && CAs=("-CApath" "$2")
+  CUT="$1"
+  shift 2
+  debug "Checking Cert: openssl verify ${CAs[@]} ${@/#/-untrusted } $CUT"
+  openssl verify ${CAs[@]} ${@/#/-untrusted } "$CUT" >/dev/null
+}
+
+# Gets all certs as published in Openssl Handshake as multiPEM
+#  getServerCert <servername> [<IPaddr>]
+#
+getServerCert () {
+  SERVER="${2:-$1}"
+  debug "Obtaining TSL Cert for host: $SERVER at $1:443"
+  [ "$DEBUG" ] && echo "Obtaining TSL Cert for host: $SERVER at $1:443" >&2
+  echo | openssl s_client -servername "$SERVER" -showcerts -connect "$1:443" 2>/dev/null |awk 'BEGIN {a=0} /^-----BEGIN CERTIFICATE-----$/ {a=1} {if (a==1) print $0}  /^-----END CERTIFICATE-----$/ {a=0}'
+}
+
+# Check file is formatted PEM, DER or concatenated PEM
+#   checkCertFormat <pathToFile>
+# 
+checkCertFormat () {
+  NPEMS=`awk 'BEGIN {a=0;v=0} /^-----BEGIN CERTIFICATE-----$/ {a++;data=""} {b=sprintf("%03i",a/2); if (a%2) data=data $0 "\n"}  /^-----END CERTIFICATE-----$/ {a++; print data |& "openssl x509 -noout" ; ret=close ("openssl x509 -noout") ; if (ret==0) v++} END {print v}' "$1"` || return 127
+  debug "NPEMS in $1 = $NPEMS"
+  [ $NPEMS -eq 1 ] && echo PEM && return 0
+  [ $NPEMS -gt 1 ] && echo multiPEM && return 0
+  openssl x509 -in "$1" -inform DER -noout 2>/dev/null && echo DER && return 0
+  echo UNKNOWN
+  return 1
+}
+
+# Make a temp file / dir
+#   mktempfile mktempdir
+#   creates file / directory in CWD to a specific format and sends the name to STDOUT
+mktempfile () {
+  mktemp -p. acme.le.tmp.file.XXXXXXXX
+}
+
+mktempdir () { 
+  mktemp -d -p. acme.le.tmp.dir.XXXXXXXX
+}
+
+# Delete temp file / dir as made by mktempfile / mktempdir
+# Checks format before issuing delete commands; returns 0 if successful
+deltempfile () {
+  debug "Attempting to remove '$1'"
+  echo "$1" | grep -q '\(^\|/\)acme\.le\.tmp\.file\.[a-zA-Z0-9]\{8\}$' && [ -f "$1" ] && [ -r "$1" ] || return 1
+  rm "$1"
+}
+
+deltempdir () {
+  debug "Attempting to remove '$1'; PWD is `pwd`"
+  echo "$1" | grep -q '\(^\|/\)acme\.le\.tmp\.dir\.[a-zA-Z0-9]\{8\}$' && [ -d "$1" ] || return 1
+  rm -rf "$1"
+}
+
+
