c9b189e57cfe1f8061bc4a4bd9a65103ba3f7bba
nimpo <dr.mike.jones@gmail.com>
2023-01-19 11:46:06 +0000

    Tweek checkFile; Add comments to be clear.

--- acme.functions.sh
+++ acme.functions.sh
@@ -166,16 +166,17 @@ function generateCSR () { # Generates a CSR > $1.csr using config in $1.cnf
 }
 
 function checkFile () { # Takes a string--a path--to an existing or soon to be existing file and attempts to check it can be written to
-  [ -z "$1" ] && echo "expecitng a string in checkFile" && return 1
-  echo "$1" |grep -q '^[^/]' && echo "expecitng full path not local path $1" && return 1 # This is a local filename
-  echo "$1" |grep -q '^\.\{1,2\}/' && echo "Path cannot be relative in $1" && return 1 # path is relative to this path
-  echo "$1" |grep -q '/\.\./' && echo "Path cannot have relative components in $1" # path has relative components
-  [ -L "$1" ] && echo "Leaf symbolic links not supported. $1 is a symbolic link" && return 1
-  [ -d "$1" ] && echo "Directory found $1 when expecting filename" && return 1
-  [ -f "$1" ] && [ ! -w "$1" ] && echo "Can't write to existing $1" && return 1
-  [ ! -e "$1" ] && touch "$1" && rm "$1" && return 0
-  [ ! -f "$1" ] && echo "$1 is not a file." && return 1
-  return 0
+  [ -z "$1" ] && debug "expecitng a string in checkFile" && return 1                           # Is input provided
+  echo "$1" |grep -q '^[^/]' && debug "expecitng full path not local path $1" && return 1      # Input should be a path string with ^/
+  echo "$1" |grep -q '^\.\{1,2\}/' && debug "Path cannot be relative in $1" && return 1        # Path is relative to this path (should not get here if is)
+  echo "$1" |grep -q '/\.\./' && debug "Path cannot have relative components in $1"            # Path has relative components
+  [ -L "$1" ] && debug "Leaf symbolic links not supported. $1 is a symbolic link" && return 1  # Not a symbplic link in basename
+  [ -d "$1" ] && debug "Directory found $1 when expecting filename" && return 1                # Can't be a Dir
+  [ -f "$1" ] && [ ! -w "$1" ] && debug "Can't write to existing $1" && return 1               # Filesystem says unwritable
+  [ -f "$1" ] && ! touch -r "$1" "$1" && debug "Can't write to existing $1" && return 1        # File says it's unwritable
+  [ ! -e "$1" ] && touch "$1" && rm "$1" && return 0                                           # Doesn't exist but would be writable (at time of checking - not atomic)
+  [ ! -f "$1" ] && debug "$1 is not a file." && return 1                                       # Exists and is not a file
+  return 0                                                                                     # If here, path is OK for writing
 }
 
 function B64 () {
